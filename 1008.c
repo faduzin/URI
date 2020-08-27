b0aeae6c50aed77527d01a3fa4b3f05ecb162914
Éric Fadul Cunha Yoshida <70298280+faduzin@users.noreply.github.com>
2020-08-26 21:29:57 -0300

    Create 1008.c

---
+++ 1008.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int N, A;
+    float B, C;
+    scanf("%d %d %f", &N, &A, &B);
+    C = A*B;
+    printf("NUMBER = %d\nSALARY = U$ %.2f\n", N, C);
+    return 0;
+}
