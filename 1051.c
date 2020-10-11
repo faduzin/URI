3e600ac7a9062f4a8c8b916675a163e4ed38de35
Éric Fadul Cunha Yoshida <70298280+faduzin@users.noreply.github.com>
2020-10-11 17:53:41 -0300

    Create 1051.c

---
+++ 1051.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    float a;
+    scanf("%f", &a);
+    if(a >= 0 && a <= 2000.0){
+        printf("Isento\n");
+    }
+    else if(a > 2000.0 && a <= 3000.0){
+        printf("R$ %.2f\n", (a-2000.0)*0.08);
+    }
+    else if(a > 3000.0 && a <= 4500.0){
+        printf("R$ %.2f\n", (1000.0*0.08)+((a-3000.0)*0.18));
+    }
+    else if(a > 4500.0){
+        printf("R$ %.2f\n", (1000.0*0.08)+(1500.0*0.18)+((a-4500.0)*0.28));
+    }
+    return 0;
+}
